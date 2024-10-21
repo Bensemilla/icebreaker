5bd7e50bc4c605480acd11b816d12eedb4f83509
Benjamin Schroeder <benjamin.schroeder587@gmail.com>
2024-10-21 11:19:41 +0200

    Added function for random question generation and root GUI window

--- icebreaker.py
+++ icebreaker.py
@@ -1,5 +1,6 @@
 import random
 import openpyxl
+from tkinter import *
 
 # Save the excel file questions in a list
 
@@ -14,5 +15,18 @@ for row in sheet:
 questions_length = len(questions_list)
 
 
+#Define function to retrieve random question
 
-print(questions_length)
+def random_question():
+    question_num = random.randint(0, (questions_length - 1))
+    print(questions_list[question_num])
+
+
+# Create and define root GUI window
+
+root_window = Tk()
+root_window.title("Icebreaker questions")
+root_window.geometry('350x200')
+
+root_window.mainloop()
+
